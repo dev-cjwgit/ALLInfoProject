8d7565159a0d132087eccb5d0303a688b8933c2c
최진우 <cjw_git@naver.com>
2022-11-28 20:38:35 +0900

    signup validation update

--- src/main/java/com/allinfo/model/domain/UserDTO.java
+++ src/main/java/com/allinfo/model/domain/UserDTO.java
@@ -25,7 +25,7 @@ public class UserDTO implements UserDetails {
 
     @NotNull
     @Size(min = 6, max = 20, groups = {ValidationGroups.signup.class}, message = "아이디는 6글자이상 20글자 이하입니다.")
-    @Pattern(regexp = "^[a-zA-Z가-힣0-9]{1,20}$", groups = {ValidationGroups.signup.class}, message = "아이디는 특수문자와 초성은 사용불가능합니다")
+    @Pattern(regexp = "^[a-zA-Z0-9]{6,20}$", groups = {ValidationGroups.signup.class}, message = "아이디는 특수문자와 초성, 한글은 사용불가능합니다")
     protected String id;
     @NotNull(groups = {ValidationGroups.signup.class}, message = "비밀번호는 공백일 수 없습니다.")
     @Size(min = 8, max = 30, groups = {ValidationGroups.signup.class}, message = "비밀번호는은 8글자 이상 30글자 이하입니다.")
