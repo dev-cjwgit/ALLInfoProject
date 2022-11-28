c05c23818636426f433a9d02c66dc797568f7609
최진우 <cjw_git@naver.com>
2022-11-28 20:39:12 +0900

    login validation update

--- src/main/java/com/allinfo/model/domain/param/LoginDTO.java
+++ src/main/java/com/allinfo/model/domain/param/LoginDTO.java
@@ -11,7 +11,7 @@ import javax.validation.constraints.Size;
 public class LoginDTO {
     @NotNull
     @Size(min = 6, max = 20, groups = {ValidationGroups.signup.class}, message = "아이디는 6글자이상 20글자 이하입니다.")
-    @Pattern(regexp = "^[a-zA-Z가-힣0-9]{1,20}$", groups = {ValidationGroups.signup.class}, message = "아이디는 특수문자와 초성은 사용불가능합니다")
+    @Pattern(regexp = "^[a-zA-Z0-9]{6,20}$", groups = {ValidationGroups.signup.class}, message = "아이디는 특수문자와 초성, 한글은 사용불가능합니다")
     private String id;
 
     @NotNull(groups = {ValidationGroups.signup.class}, message = "비밀번호는 공백일 수 없습니다.")
